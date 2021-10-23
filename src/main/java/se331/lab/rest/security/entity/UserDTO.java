a137c09c3b5f3db1c54e4985f4c63dc2e74d341b
unknown <peerapat.next@gmail.com>
2021-10-23 14:34:20 +0700

    initial commit

---
+++ src/main/java/se331/lab/rest/security/entity/UserDTO.java
@@ -0,0 +1,16 @@
+package se331.lab.rest.security.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    String username;
+    String password;
+    String email;
+}
