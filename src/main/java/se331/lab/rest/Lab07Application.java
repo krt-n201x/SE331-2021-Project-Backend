8eeeadde5e8062f084bb36521ce8b24c69040d0e
Kitsada <kitsada_t@cmu.ac.th>
2021-10-27 12:26:25 +0700

    Deploy again

--- src/main/java/se331/lab/rest/Lab07Application.java
+++ src/main/java/se331/lab/rest/Lab07Application.java
@@ -20,6 +20,7 @@ public class Lab07Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://54.90.156.250:8001")
                         .exposedHeaders("x-total-count");
             }
         };
