d43adc7cdfd677d3e0d519e5a4582d65f5d3509a
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-10-27 07:56:33 +0700

    add user

--- src/main/java/se331/lab/rest/config/InitApp.java
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -38,7 +38,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     @Override
     @Transactional
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        Patients pat1,pat2,pat3;
+        Patients pat1,pat2,pat3,pat4, pat5, pat6, pat7, pat8, pat9;
         Doctor doc1,doc2,doc3;
         pat1 = patientRepository.save(Patients.builder()
                 .name("John")
@@ -61,6 +61,49 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .age(31)
                 .hometown("Bangkok, Thailand")
                 .doctor_comm("something bangsat").build());
+        pat4 = patientRepository.save(Patients.builder()
+                .name("Break")
+                .surname("Fast")
+                .status("Taken 2 doses")
+                .age(56)
+                .hometown("Bangkok, Thailand")
+                .doctor_comm("yes okay").build());
+        pat5 = patientRepository.save(Patients.builder()
+                .name("Morty")
+                .surname("Sanchez")
+                .status("Taken 1 doses")
+                .age(15)
+                .hometown("Bangkok, Thailand")
+                .doctor_comm("I love pickle").build());
+        pat6 = patientRepository.save(Patients.builder()
+                .name("Noah")
+                .surname("Titanic")
+                .status("Taken 2 doses")
+                .age(100)
+                .hometown("Bangkok, Thailand")
+                .doctor_comm("drowning").build());
+        pat7 = patientRepository.save(Patients.builder()
+                .name("Katy")
+                .surname("Perry")
+                .status("Taken 1 doses")
+                .age(58)
+                .hometown("Bangkok, Thailand")
+                .doctor_comm("hot and cold").build());
+        pat8 = patientRepository.save(Patients.builder()
+                .name("Chick")
+                .surname("Ken")
+                .status("Taken 1 doses")
+                .age(2)
+                .hometown("Bangkok, Thailand")
+                .doctor_comm("high protein").build());
+        pat9 = patientRepository.save(Patients.builder()
+                .name("Sushi")
+                .surname("Maki")
+                .status("Taken 1 doses")
+                .age(44)
+                .hometown("Bangkok, Thailand")
+                .doctor_comm("Yummy yum").build());
+
         doc1 = doctorRepository.save(Doctor.builder()
                 .name("Peerapat")
                 .surname("Thungngoen")
@@ -79,6 +122,13 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         pat1.setDoctor(doc1);
         pat2.setDoctor(doc2);
         pat3.setDoctor(doc3);
+        pat4.setDoctor(doc1);
+        pat5.setDoctor(doc2);
+        pat6.setDoctor(doc3);
+        pat7.setDoctor(doc1);
+        pat8.setDoctor(doc2);
+        pat9.setDoctor(doc3);
+
         pat1.getImageUrl().add("https://www.img.in.th/images/30ace35710f11b4a7f21f7295e508df5.png");
         pat2.getImageUrl().add("https://www.img.in.th/images/30ace35710f11b4a7f21f7295e508df5.png");
         pat3.getImageUrl().add("https://www.img.in.th/images/a2bf62a37f1666840b96d4b5b636dadf.png");
@@ -113,6 +163,57 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .build());
         tempVac.setPatient(pat3);
         pat3.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Sinovac")
+                .date_injected("August 22 2021")
+                .build());
+        tempVac.setPatient(pat4);
+        pat4.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Sinovac")
+                .date_injected("August 30 2021")
+                .build());
+        tempVac.setPatient(pat4);
+        pat4.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Sinovac")
+                .date_injected("October 22 2021")
+                .build());
+        tempVac.setPatient(pat5);
+        pat5.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Sinovac")
+                .date_injected("August 7 2021")
+                .build());
+        tempVac.setPatient(pat6);
+        pat6.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Moderna")
+                .date_injected("August 15 2021")
+                .build());
+        tempVac.setPatient(pat6);
+        pat6.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Moderna")
+                .date_injected("Janury 2 2021")
+                .build());
+        tempVac.setPatient(pat7);
+        pat7.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Sinovac")
+                .date_injected("Febuary 14 2021")
+                .build());
+        tempVac.setPatient(pat8);
+        pat8.getVaccine().add(tempVac);
+        tempVac = vaccineRepository.save(Vaccine.builder()
+                .name("Moderna")
+                .date_injected("August 22 2021")
+                .build());
+        tempVac.setPatient(pat9);
+        pat9.getVaccine().add(tempVac);
+
+
+
         addUser();
         doc1.setUser(user3);
         user3.setDoctor(doc1);
@@ -126,8 +227,20 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         user5.setPatient(pat3);
         doc3.setUser(user6);
         user6.setDoctor(doc3);
+        pat4.setUser(user7);
+        user7.setPatient(pat4);
+        pat5.setUser(user8);
+        user8.setPatient(pat5);
+        pat6.setUser(user9);
+        user9.setPatient(pat6);
+        pat7.setUser(user10);
+        user10.setPatient(pat7);
+        pat8.setUser(user11);
+        user11.setPatient(pat8);
+        pat9.setUser(user12);
+        user12.setPatient(pat9);
     }
-    User user1, user2, user3, user4, user5, user6;
+    User user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12;
     private void addUser(){
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         Authority authUser = Authority.builder().name(AuthorityName.ROLE_USER).build();
@@ -187,6 +300,61 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .enabled(true)
                 .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
                 .build();
+        user7 = User.builder()
+                .username("user5")
+                .password(encoder.encode("user5"))
+                .firstname("user5")
+                .lastname("user5")
+                .email("enabled5@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        user8 = User.builder()
+                .username("user6")
+                .password(encoder.encode("user6"))
+                .firstname("user6")
+                .lastname("user6")
+                .email("enabled6@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        user9 = User.builder()
+                .username("user7")
+                .password(encoder.encode("user7"))
+                .firstname("user7")
+                .lastname("user7")
+                .email("enabled7@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        user10 = User.builder()
+                .username("user8")
+                .password(encoder.encode("user8"))
+                .firstname("user8")
+                .lastname("user8")
+                .email("enabled8@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        user11 = User.builder()
+                .username("user9")
+                .password(encoder.encode("user9"))
+                .firstname("user9")
+                .lastname("user9")
+                .email("enabled9@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        user12 = User.builder()
+                .username("user10")
+                .password(encoder.encode("user10"))
+                .firstname("user10")
+                .lastname("user10")
+                .email("enabled10@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+
         authorityRepository.save(authUser);
         authorityRepository.save(authAdmin);
         authorityRepository.save(authDoctor);
@@ -198,12 +366,24 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         user4.getAuthorities().add(authUser);
         user5.getAuthorities().add(authUser);
         user6.getAuthorities().add(authUser);
+        user7.getAuthorities().add(authUser);
+        user8.getAuthorities().add(authUser);
+        user9.getAuthorities().add(authUser);
+        user10.getAuthorities().add(authUser);
+        user11.getAuthorities().add(authUser);
+        user12.getAuthorities().add(authUser);
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
         userRepository.save(user4);
         userRepository.save(user5);
         userRepository.save(user6);
+        userRepository.save(user7);
+        userRepository.save(user8);
+        userRepository.save(user9);
+        userRepository.save(user10);
+        userRepository.save(user11);
+        userRepository.save(user12);
 
     }
 }
